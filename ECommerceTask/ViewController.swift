07e85cd00bea2cb7d9a135ca70622af241fc673a
Ugur Cetinkaya <ugurcetinkaya@ymail.com>
2017-11-02 11:46:10 +0300

    Initial Commit

---
+++ ECommerceTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECommerceTask
+//
+//  Created by Ugur Cetinkaya on 2.11.2017.
+//  Copyright © 2017 Ugur Cetinkaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
