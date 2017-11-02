c8f0d585d713f8f222046025df369acc71cf41f9
Ugur Cetinkaya <ugurcetinkaya@ymail.com>
2017-11-02 12:17:13 +0300

    Folder structure is made

---
+++ ECommerceTask/Views/ProductListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProductListViewController.swift
+//  ECommerceTask
+//
+//  Created by Ugur Cetinkaya on 2.11.2017.
+//  Copyright © 2017 Ugur Cetinkaya. All rights reserved.
+//
+
+import UIKit
+
+class ProductListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
